f5ca75e33b4521cc480f20133df9464002ff73d6
ivakula <dfd@gmail.com>
2021-11-17 12:06:08 +0200

    homework13

---
+++ src/homework13/FileData.java
@@ -0,0 +1,34 @@
+package homework13;
+
+public class FileData {
+    private final String name;
+    private final long size;
+    private final String path;
+
+    public FileData(String name, long size, String path) {
+        this.name = name;
+        this.size = size;
+        this.path = path;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    @Override
+    public String toString() {
+        return "FileData{" +
+                "name='" + name + '\'' +
+                ", size=" + size +
+                ", path='" + path + '\'' +
+                '}';
+    }
+}
